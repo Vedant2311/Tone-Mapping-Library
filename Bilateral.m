564c76ef57f12831ffa7c0deef8a90954dac1ef5
Vedant Raval <32142369+Vedant2311@users.noreply.github.com>
2019-12-02 23:27:09 +0800

    Add files via upload

---
+++ Bilateral.m
@@ -0,0 +1,145 @@
+Img = hdrread('memorial.hdr');
+[r,c,h] = size(Img);
+
+Lum = zeros(r,c);
+
+for i=1:r
+    for j=1:c
+        Lum(i,j) = Img(i,j,1)*0.299 + Img(i,j,2)*0.587 + Img(i,j,3)*0.114;
+    end
+end
+
+H = zeros(r,c);
+
+for i=1:r
+    for j = 1:c
+        H(i,j) = log10(Lum(i,j));
+    end
+end
+
+gradH = zeros(r,c,2);
+
+for i=1:r
+    for j=1:c
+       
+        
+        if i==1 
+           A = H(i+1,j);
+           B=0;
+        
+        elseif i==r
+            A = 0;
+            B= H(i-1,j);
+        
+        else
+            
+            A= H(i+1,j);
+            B = H(i-1,j);
+        end
+        
+        if j==1 
+            
+            C = H(i,j+1);
+            D = 0;
+        elseif j==c
+            
+            C = 0;
+            D = H(i,j-1);
+        else
+            
+            C = H(i,j+1);
+            D = H(i,j-1);
+        end
+        
+        x = (A-B)/2;
+        y = (C-D)/2;
+        gradH(i,j,:) = [x,y];
+       
+    end
+end
+
+sum = 0;
+for i=i:r
+    for j=1:c
+        
+        sum = sum + sqrt(gradH(i,j,1)^2 + gradH(i,j,2)^2);
+    end
+end
+
+average = sum/r*c;
+alpha = 0.1*average;
+beta = 0.85;
+
+phi = zeros(r,c);
+
+for i=1:r
+    for j=1:c
+        
+        phi(i,j) = (alpha/sqrt(gradH(i,j,1)^2 + gradH(i,j,2)^2)) * (sqrt(gradH(i,j,1)^2 + gradH(i,j,2)^2)/alpha)^(beta);
+        
+    end
+end
+
+G = gradH .* phi;
+
+divG = zeros(r,c);
+
+for i=1:r
+    for j=1:c
+        
+        if ((i==1) && (j==1))
+            
+            divG(i,j) = G(i,j,1) + G(i,j,2);
+        elseif i==1
+            
+            divG(i,j) = G(i,j,1) + G(i,j,2) - G(i,j-1,2);
+        elseif j==1
+            
+            divG(i,j) = G(i,j,1) - G(i-1,j,1) + G(i,j,2);
+        else
+            
+            divG(i,j) = G(i,j,1) - G(i-1,j,1) + G(i,j,2) - G(i,j-1,2);
+        end
+    end
+end
+divG = fillmissing(divG,'previous');
+f = fft(divG);
+
+fi = zeros(r,c);
+Lx = r;
+Ly = c;
+
+for i=1:r
+    for j = 1:c
+        
+        fi(i,j) = (f(i,j))/(((2*pi*(i/Lx)*(1j))^2)+((2*pi*(j/Ly)*(1j))^2));
+        
+    end
+end
+
+I = real(ifft(fi));
+
+for i=1:r
+    for j=1:c
+        I(i,j) = 10^(I(i,j));
+        if j==1 
+            I(i,j) = 0;
+        end
+        
+    end
+end
+
+ImgOutLum = zeros(r,c,h);
+
+for i = 1:r
+    for j = 1:c
+        for k =1:h
+            ImgOutLum(i,j,1) = (((Img(i,j,1))/(Lum(i,j))))^(0.5)*(I(i,j));
+            ImgOutLum(i,j,2) = (((Img(i,j,2))/(Lum(i,j))))^(0.5)*(I(i,j));
+            ImgOutLum(i,j,3) = (((Img(i,j,3))/(Lum(i,j))))^(0.5)*(I(i,j));
+        end
+    end
+end
+
+imshow(ImgOutLum);
+
---
+++ Reinhard.m
@@ -0,0 +1,157 @@
+Img = hdrread('memorial.hdr');
+[r,c,h] = size(Img);
+
+Lum = zeros(r,c);
+
+for i=1:r
+    for j=1:c
+        Lum(i,j) = Img(i,j,1)*0.299 + Img(i,j,2)*0.587 + Img(i,j,3)*0.114;
+    end
+end
+
+delta = 0.00001;
+sum = 0;
+for i=1:r
+    for j=1:c
+        sum = sum + log(delta + Lum(i,j));
+    end
+end
+
+Lw = exp(sum/(r*c));
+
+L = zeros(r,c);
+
+a = 0.045;
+
+for i=1:r
+    for j=1:c
+        L(i,j) = (a/Lw)*Lum(i,j);
+    end
+end
+
+alpha1 = 1/(2*(sqrt(2)));
+alpha2 = 1.6*alpha1;
+
+epsilon = 0.05;
+phi = 15.0;
+
+
+%R1 = (1./(pi*(alpha1*s).^2))*(exp(-(x.^2 + y.^2)./(alpha1 * s).^2));
+R1 = zeros(r,c,9);
+R2 = zeros(r,c,9);
+
+
+for x=1:r
+    for y=1:c
+        for s=1:9
+            sS = 1.6^(s-1);
+            R1(x,y,s) = (1/(pi*(alpha1*sS)^2))*(exp(-(x^2 + y^2)/(alpha1 * sS)^2));
+            R2(x,y,s) = (1/(pi*(alpha2*sS)^2))*(exp(-(x^2 + y^2)/(alpha2 * sS)^2));
+        end
+    end
+end
+
+for s=1:9
+   
+    V1(:,:,s) = ifft(fft(L).*fft(R1(:,:,s)));
+    V2(:,:,s) = ifft(fft(L).*fft(R1(:,:,s)));
+    
+    
+end
+
+%V1 = convn(R1,L,'same');
+%V2 = convn(R2,L,'same');
+
+V = zeros(r,c,9);
+
+for x=1:r
+    for y=1:c
+        for s=1:9
+            sS = 1.6^(s-1);
+            V(x,y,s) = (V1(x,y,s)-V2(x,y,s))/(((2^phi)*((a)/(sS*sS)))+V1(x,y,s));
+        end
+    end
+end
+
+Ld = zeros(r,c);
+
+for i=1:r
+    for j=1:c
+        
+        sS = 1;
+        for s=1:9
+            
+            if (abs(V(i,j,s)))<epsilon
+                break;
+            end
+            
+            if s~=9
+                sS = sS * 1.6;
+            end
+        end
+        
+        
+        p = 1 + round(log(sS)/log(1.6));
+        
+        Ld(i,j) = L(i,j)/(1+V1(i,j,p));
+        
+    end
+end
+
+ImgOut = zeros(r,c,h);
+
+for i = 1:r
+    for j = 1:c
+        for k =1:h
+            ImgOut(i,j,1) = (((Img(i,j,1))/(Lum(i,j))))*(Ld(i,j));
+            ImgOut(i,j,2) = (((Img(i,j,2))/(Lum(i,j))))*(Ld(i,j));
+            ImgOut(i,j,3) = (((Img(i,j,3))/(Lum(i,j))))*(Ld(i,j));
+        end
+    end
+end
+
+imshow(ImgOut);
+
+function A = unscaledGamma(ImgOutSimple,r,c,h)
+
+    ImgOutGamma = zeros(r,c,h);
+    
+    for i=1:r
+        for j = 1:c
+            for k = 1:h
+               if (ImgOutSimple(i,j,k))<=0.0031308 
+                  ImgOutGamma(i,j,k) = 12.92*(ImgOutSimple(i,j,k)); 
+               else
+                   ImgOutGamma(i,j,k) =  (((ImgOutSimple(i,j,k))^(1/2.4))*1.055 + (-0.055))*1;
+               end
+            end
+        end
+    end
+
+    A = ImgOutGamma;
+
+end
+
+function A = gamma(ImgOutSimple,r,c,h)
+
+    ImgOutGamma = zeros(r,c,h);
+    
+    for i=1:r
+        for j = 1:c
+            for k = 1:h
+               if (ImgOutSimple(i,j,k)/255)<=0.0031308 
+                  ImgOutGamma(i,j,k) = 12.92*(ImgOutSimple(i,j,k)/255); 
+               else
+                   ImgOutGamma(i,j,k) =  (((ImgOutSimple(i,j,k)/255)^(1/2.4))*1.055 + (-0.055))*1;
+               end
+
+               ImgOutGamma(i,j,k) = ImgOutGamma(i,j,k)*255;
+
+            end
+        end
+    end
+
+    A = ImgOutGamma;
+
+end
+
