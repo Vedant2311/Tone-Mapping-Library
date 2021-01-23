7dca544ca4dfa3aa65c106ee035c3c8c96eaaeb0
Vedant Raval <32142369+Vedant2311@users.noreply.github.com>
2021-01-23 20:07:45 +0530

    Update Reinhard.m

--- Reinhard.m
+++ Reinhard.m
@@ -1,14 +1,16 @@
+% Getting the Input image
 Img = hdrread('memorial.hdr');
 [r,c,h] = size(Img);
 
+% Luminance matrix
 Lum = zeros(r,c);
-
 for i=1:r
     for j=1:c
         Lum(i,j) = Img(i,j,1)*0.299 + Img(i,j,2)*0.587 + Img(i,j,3)*0.114;
     end
 end
 
+% Performing the required operations on the Luminance values
 delta = 0.00001;
 sum = 0;
 for i=1:r
@@ -18,9 +20,7 @@ for i=1:r
 end
 
 Lw = exp(sum/(r*c));
-
 L = zeros(r,c);
-
 a = 0.045;
 
 for i=1:r
@@ -29,17 +29,14 @@ for i=1:r
     end
 end
 
+% Defining the constants as directed in the paper
 alpha1 = 1/(2*(sqrt(2)));
 alpha2 = 1.6*alpha1;
-
 epsilon = 0.05;
 phi = 15.0;
 
-
-%R1 = (1./(pi*(alpha1*s).^2))*(exp(-(x.^2 + y.^2)./(alpha1 * s).^2));
 R1 = zeros(r,c,9);
 R2 = zeros(r,c,9);
-
 
 for x=1:r
     for y=1:c
@@ -51,19 +48,13 @@ for x=1:r
     end
 end
 
+% Performing the required Fourier Transforms
 for s=1:9
-   
     V1(:,:,s) = ifft(fft(L).*fft(R1(:,:,s)));
     V2(:,:,s) = ifft(fft(L).*fft(R1(:,:,s)));
-    
-    
 end
 
-%V1 = convn(R1,L,'same');
-%V2 = convn(R2,L,'same');
-
 V = zeros(r,c,9);
-
 for x=1:r
     for y=1:c
         for s=1:9
@@ -90,16 +81,14 @@ for i=1:r
             end
         end
         
-        
         p = 1 + round(log(sS)/log(1.6));
-        
         Ld(i,j) = L(i,j)/(1+V1(i,j,p));
         
     end
 end
 
+% Getting the output Image using the transformations done earlier
 ImgOut = zeros(r,c,h);
-
 for i = 1:r
     for j = 1:c
         for k =1:h
@@ -109,9 +98,9 @@ for i = 1:r
         end
     end
 end
-
 imshow(ImgOut);
 
+% The unscaled Gamma function
 function A = unscaledGamma(ImgOutSimple,r,c,h)
 
     ImgOutGamma = zeros(r,c,h);
@@ -132,6 +121,7 @@ function A = unscaledGamma(ImgOutSimple,r,c,h)
 
 end
 
+% The scaled gamma function
 function A = gamma(ImgOutSimple,r,c,h)
 
     ImgOutGamma = zeros(r,c,h);
@@ -154,4 +144,3 @@ function A = gamma(ImgOutSimple,r,c,h)
     A = ImgOutGamma;
 
 end
-
